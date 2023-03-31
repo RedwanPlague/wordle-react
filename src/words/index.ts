72c44cdb41b32eec712d5db27ccd4167eec57517
RedwanPlague <mdredwan.rh@gmail.com>
2023-03-31 20:17:33 +0600

    Add word list and methods

---
+++ src/words/index.ts
@@ -0,0 +1,27 @@
+import words from "./words";
+
+const MinWordLength = 3;
+const MaxWordLength = words.length - 1;
+
+const getRandomLength = () => {
+  return (
+    MinWordLength +
+    Math.floor(Math.random() * (MaxWordLength - MinWordLength + 1))
+  );
+};
+
+const getRandomWord = (length: number = 0) => {
+  if (length === 0) {
+    length = getRandomLength();
+    console.log(length);
+  }
+  const targets = words[length];
+  return targets[Math.floor(Math.random() * targets.length)];
+};
+
+const isWordValid = (word: string) => {
+  const length = word.length;
+  return length < words.length && words[length].includes(word);
+};
+
+export { getRandomWord, isWordValid, MinWordLength, MaxWordLength };
